30983b493da3824a14303ef44961ad13675bd4a2
Luill823 <luisia0237@gmail.com>
2025-02-12 11:34:18 -0600

    TabBar Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:donut_app_2b_illescas/pages/home_page.dart';
 import 'package:flutter/material.dart';
-
+ 
 void main() {
   runApp(const MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+ 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,71 @@
+import 'package:donut_app_2b_illescas/utils/my_tab.dart';
 import 'package:flutter/material.dart';
-
+ 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
+ 
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
+ 
 class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = [
+
+      MyTab(iconPath: "lib/icons/donut.png"),
+
+      MyTab(iconPath: "lib/icons/burger.png"),
+      
+      MyTab(iconPath: "lib/icons/pancakes.png"),
+
+      MyTab(iconPath: "lib/icons/pizza.png"),
+
+      MyTab(iconPath: "lib/icons/smoothie.png"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: Icon(
-          Icons.menu,
-          color: Colors.grey[800], // <-- Aquí agregamos la coma
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          //icono izquierdo
+          leading: Icon(
+            Icons.menu,
+            color: Colors.grey[800]),
+          //icodo derecho
+          actions: [Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Icon(Icons.person),
+          )],
         ),
-        actions: [
-          Icon(Icons.person),
-        ], // <-- Aquí agregamos la coma después del último elemento de la lista
+       
+       
+        body: Column(children: [
+          //texto
+        Padding(padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18))
+        ,Row(
+          children: [
+            Text("I Want to", style: TextStyle( fontSize: 30 )),
+            Text(" eat", style: TextStyle(
+               fontSize: 30 ,
+               fontWeight: FontWeight.bold,
+               decoration: TextDecoration.underline))
+          ],
+        ),
+       
+      //tab bar
+       TabBar(tabs: myTabs)
+       
+      //tab bar view
+       
+       
+       
+       
+      // carrito
+       
+       
+        ],)
       ),
     );
   }
